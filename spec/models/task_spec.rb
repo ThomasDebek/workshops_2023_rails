c26a1630c618dba63b702e316cfedec4216c830f
ThomasDebek <tom.debek@gmail.com>
2023-03-31 23:30:27 +0200

    rspec:  test models passed

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  subject(:task) { described_class.new(title: title) }
+  subject(:task) { described_class.new(title: title, description: description, deadline: deadline) }
+
 
   context 'when title indicated' do
     let(:title) { 'Feed your dog' }
+    let(:description) { 'Ala ma kota' }
+    let(:deadline) { '2023-03-31' }
 
     specify { expect(task).to be_valid }
   end
 
   context 'when title is empty' do
     let(:title) { nil }
+    let(:description) { 'Ala ma kota' }
+    let(:deadline) { '2023-03-31' }
 
     specify { expect(task).to be_invalid }
   end
