08d911201ff552621ee01ab7ba6d80eb30340921
ThomasDebek <tom.debek@gmail.com>
2023-04-03 13:34:29 +0200

    postgresql: change system data_base

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_31_220119) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "realizations", force: :cascade do |t|
     t.datetime "done_at"
-    t.integer "task_id", null: false
+    t.bigint "task_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_realizations_on_task_id"
